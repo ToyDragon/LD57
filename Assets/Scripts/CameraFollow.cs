0c7208fda83003750a8bc65c201fe14d389dfcb3
Matt Bates <batesmatthewj@gmail.com>
2025-04-05 22:53:43 -0500

    zoom camera out a bit and add fan to demo scene

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -1,9 +1,10 @@
 using UnityEngine;
 
 public class CameraFollow : MonoBehaviour {
+    public float followDist = 12f;
     void LateUpdate() {
         var ship = ShipMovement.instance.transform;
-        transform.localPosition = .8f*ship.localPosition + Vector3.back * 12f - Vector3.Dot(ship.right, Vector3.right)*Vector3.right*2;
+        transform.localPosition = .8f*ship.localPosition + Vector3.back * followDist - Vector3.Dot(ship.right, Vector3.right)*Vector3.right*2;
         transform.rotation = Quaternion.Lerp(Quaternion.identity, ship.rotation, .05f);
     }
 }
