9ac7350f51233210b903cbcff0e3d060798d9215
Matt Bates <batesmatthewj@gmail.com>
2025-04-07 19:50:03 -0500

    die once

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -4,12 +4,14 @@ public class EnemyHealth : MonoBehaviour
 {
     public static int killsThisLevel = 0;
     public int life;
+    private bool dead;
     void Update() {
         
     }
     public void Hit() {
         life -= 1;
-        if (life < 0) {
+        if (life < 0 && !dead) {
+            dead = true;
             Destroy(gameObject);
             killsThisLevel++;
         }
